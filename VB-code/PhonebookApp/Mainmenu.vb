ccfb646df426dc4dcb1576228554ed7068ac4cb1
roderickalew <roderickalew@gmail.com>
2021-10-17 16:34:55 -0600

    checking changes

--- VB-code/PhonebookApp/Mainmenu.vb
+++ VB-code/PhonebookApp/Mainmenu.vb
@@ -1,16 +1,25 @@
+Imports System.IO
+Imports System.Text.Json
 Class MainMenu
     Implements IMenu
+    dim friends as List(of Contact) = new List(of Contact)
+    dim filename as string = "friends.json"
+    dim jsonstring as string = ""
     Sub Initial() Implements IMenu.Start
         Console.WriteLine("Hello World!")
         dim repeat as Boolean = true
         Do While repeat
             Console.WriteLine("What would you like to do?")
             Console.WriteLine("[0] Say hello")
+            Console.WriteLine("[1] Show all friends")
             Console.WriteLine("[x] Exit")
             Dim input as string = Console.ReadLine()
             Select Case input
                 Case "0"
                     Console.WriteLine("Hello")
+                    AddFriend()
+                Case "1"
+                    ShowFriends()
                 Case "x"
                     Console.WriteLine("Goodbye")
                     repeat = false
@@ -28,24 +37,22 @@ Class MainMenu
     End Sub
     Sub ShowFriends()
         Console.WriteLine("Friend list plus contact info")
-        For Each person As Contact In GetContactsFromFile()
+        For Each person As Contact In GetContacts()
             Console.WriteLine(person.ToString())
         Next
     End Sub
     Sub AddFriend2File(ByVal person as Contact)
         dim existingContacts as List(of Contact) = GetContacts()
-        dim existingContacts as List(of Contact) = GetContactsFromFile()
         existingContacts.Add(person)
         jsonstring = JsonSerializer.Serialize(existingContacts)
         File.WriteAllText(filename, jsonstring)
     End Sub
-    Function GetContactsFromFile() As List(of Contact)
+    Function GetContacts() As List(of Contact)
         Try
             jsonString = File.ReadAllText(filename)
             return JsonSerializer.Deserialize(of List(of Contact))(jsonstring)
         Catch ex As Exception
             return new List(of Contact)
         End Try
-        
     End Function
 End Class
